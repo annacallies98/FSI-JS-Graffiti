f3b6fafb44c76403d2d010c1e353691edf7a869a
Anna Callies <annacallies98@gmail.com>
2022-04-05 19:40:19 -0500

    Added DOM styling

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+header.style.color = 'pink'
+console.log(header)
 
-// Select some elements...
+
+let dogImages = document.querySelectorAll('.dog-image')
+console.log(dogImages)
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'pink'
+footer.style.borderRadius = '80px'
